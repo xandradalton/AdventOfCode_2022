b30830ca5844b9fc8908e43db689463807aded67
Alexandra Dalton <allie@Alexandras-MBP.local>
2022-12-05 15:34:18 -0500

    Finish d3 - pt2

--- d03/pt2/dist/index.js
+++ d03/pt2/dist/index.js
@@ -29,39 +29,43 @@ const rawData = fs.readFileSync(inputFile || 'inputTest.txt', 'utf8');
 const data = rawData.split('\n');
 // console.log(data)
 let totalPriority = 0;
-let itemArray = [];
-for (let i = 0; i < data.length; i++) {
-    let sackArray = data[i].split("");
-    //     console.log(sackArray)
-    let half = Math.ceil(sackArray.length / 2);
-    let firstHalf = sackArray.splice(0, half);
-    let secondHalf = sackArray.splice(-half);
-    //     console.log(firstHalf)
-    let misItem = 'A';
-    for (let i = 0; i < firstHalf.length; i++) {
-        for (let j = 0; j < secondHalf.length; j++) {
-            if (firstHalf[i] == secondHalf[j]) {
-                misItem = firstHalf[i];
-                itemArray.push(misItem);
+let badgeArray = [];
+for (let i = 0; i < data.length; i = i + 3) {
+    let group = [data[i], data[i + 1], data[i + 2]];
+    console.log(group);
+    let elf1 = group[0].split('');
+    let elf2 = group[1].split('');
+    let elf3 = group[2].split('');
+    let badge = "a";
+    for (let i = 0; i < elf1.length; i++) {
+        for (let j = 0; j < elf2.length; j++) {
+            for (let k = 0; k < elf3.length; k++) {
+                if (elf1[i] == elf2[j] && elf2[j] == elf3[k]) {
+                    badge = elf1[i];
+                    badgeArray.push(badge);
+                    break;
+                }
+            }
+            if (elf1[i] == badge) {
                 break;
             }
         }
-        if (firstHalf[i] == misItem) {
+        if (elf1[i] == badge) {
             break;
         }
     }
 }
 let alphabetArray = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
 let priorityScore = 0;
-for (let i = 0; i < itemArray.length; i++) {
+for (let i = 0; i < badgeArray.length; i++) {
     for (let j = 0; j < alphabetArray.length; j++) {
-        if (itemArray[i] == alphabetArray[j]) {
+        if (badgeArray[i] == alphabetArray[j]) {
             priorityScore = alphabetArray.indexOf(alphabetArray[j]) + 1;
             totalPriority += priorityScore;
         }
     }
 }
 console.log(alphabetArray);
-console.log(itemArray);
+console.log(badgeArray);
 console.log(totalPriority);
 //# sourceMappingURL=index.js.map
--- d03/pt2/index.ts
+++ d03/pt2/index.ts
@@ -5,45 +5,50 @@ const rawData: string = fs.readFileSync(inputFile || 'inputTest.txt', 'utf8');
 const data: string[] = rawData.split('\n');
 
 // console.log(data)
+
 let totalPriority = 0;
-let itemArray: string[] = [];
+let badgeArray: string[] = [];
 
-for (let i = 0; i < data.length; i++) {
-    let sackArray: string[] = data[i].split("");
-//     console.log(sackArray)
-    let half = Math.ceil(sackArray.length/2);
-    let firstHalf = sackArray.splice(0, half);
-    let secondHalf = sackArray.splice(-half);
-//     console.log(firstHalf)
 
-    let misItem: string = 'A';
+for (let i = 0; i < data.length; i = i + 3) {
+    let group: string[] = [data[i], data[i + 1], data[i + 2]]
+    console.log(group)
+    let elf1: string[] = group[0].split('')
+    let elf2: string[] = group[1].split('')
+    let elf3: string[] = group[2].split('')
+    let badge: string = "a"
 
-    for (let i = 0; i < firstHalf.length; i++) {
-        for (let j = 0; j < secondHalf.length; j++) {
-            if (firstHalf[i] == secondHalf[j]) {
-                misItem = firstHalf[i];
-                itemArray.push(misItem)
-                break;
+    for (let i = 0; i < elf1.length; i++) {
+        for (let j = 0; j < elf2.length; j++) {
+            for (let k = 0; k < elf3.length; k++) {
+                if(elf1[i] == elf2[j] && elf2[j] == elf3[k]) {
+                    badge = elf1[i]
+                    badgeArray.push(badge)
+                    break
+                }
+            }
+            if(elf1[i] == badge) {
+            break
             }
         }
-        if(firstHalf[i] == misItem) {
-            break;
+        if(elf1[i] == badge) {
+        break
         }
     }
 }
 
 let alphabetArray: string[] = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
-
 let priorityScore = 0;
 
-  for (let i = 0; i < itemArray.length; i++) {
+for (let i = 0; i < badgeArray.length; i++) {
     for (let j = 0; j < alphabetArray.length; j++) {
-        if(itemArray[i] == alphabetArray[j]) {
+        if(badgeArray[i] == alphabetArray[j]) {
             priorityScore = alphabetArray.indexOf(alphabetArray[j]) + 1
             totalPriority += priorityScore
         }
     }
   }
-// console.log(alphabetArray);
-// console.log(itemArray);
+
+console.log(alphabetArray);
+console.log(badgeArray);
 console.log(totalPriority);
