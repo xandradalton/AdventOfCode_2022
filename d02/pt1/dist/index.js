2613836008559c640f125f8b1796f4366b83c23a
Alexandra Dalton <allie@Alexandras-MBP.local>
2022-12-05 13:07:18 -0500

    Finish d2 - pt1

--- d02/pt1/dist/index.js
+++ d02/pt1/dist/index.js
@@ -24,7 +24,63 @@ var __importStar = (this && this.__importStar) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const fs = __importStar(require("fs"));
+// A = Rock
+// B = Paper
+// C = Scissors
+// X = Rock
+// Y = Paper
+// Z = Scissors
+// Win = 6
+// Lose = 0
+// Draw = 3
 const inputFile = process.argv[2];
 const rawData = fs.readFileSync(inputFile || 'inputTest.txt', 'utf8');
 const data = rawData.split('\n');
+// console.log(data)
+let totalScore = 0;
+for (let i = 0; i < data.length; i++) {
+    let roundArray = data[i].split(" ");
+    //     console.log(roundArray)
+    let score = 0;
+    let me = roundArray[1];
+    let them = roundArray[0];
+    if (me == "X") {
+        score++;
+        if (them == "A") {
+            score = score + 3;
+        }
+        if (them == "B") {
+            //lose
+        }
+        if (them == "C") {
+            score = score + 6;
+        }
+    }
+    if (me == "Y") {
+        score = score + 2;
+        if (them == "A") {
+            score = score + 6;
+        }
+        if (them == "B") {
+            score = score + 3;
+        }
+        if (them == "C") {
+            //lose
+        }
+    }
+    if (me == "Z") {
+        score = score + 3;
+        if (them == "A") {
+            //lose
+        }
+        if (them == "B") {
+            score = score + 6;
+        }
+        if (them == "C") {
+            score = score + 3;
+        }
+    }
+    totalScore += score;
+}
+console.log(totalScore);
 //# sourceMappingURL=index.js.map
